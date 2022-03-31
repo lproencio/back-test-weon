303c9d24a8cc69f40e7b2e61d90a9bdd19d64dd2
lproencio <leandroproencio@gmail.com>
2022-03-31 20:06:04 -0300

    create crud user

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const UserController = require("./controllers/UserController");
 
 const routes = express.Router();
 
-routes.post("/");
+routes.post("/user", UserController.createUser);
+routes.get("/user/:id", UserController.getUserById);
+routes.get("/users", UserController.getUserAll);
+routes.put("/user/:id", UserController.putUser);
+routes.delete("/user/:id", UserController.deleteUser);
 
 module.exports = routes;
